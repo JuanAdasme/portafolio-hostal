4a8c4c080a3f475a773be44f9b94ab1bd0010685
Juan Adasme <juan.adasme@oxus.cl>
2018-05-16 18:28:26 -0400

    Add header and sidebar

--- src/main/java/cl/donaclarita/portafoliohostal/controller/PageController.java
+++ src/main/java/cl/donaclarita/portafoliohostal/controller/PageController.java
@@ -4,10 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class IndexController {
+public class PageController {
 	
-	@GetMapping
+	@GetMapping("/")
+	public String root() {
+		return "index";
+	}
+	
+	@GetMapping("/index")
 	public String index() {
 		return "index";
+	}
+	
+	@GetMapping("/error")
+	public String error() {
+		return "error";
 	}
 }
