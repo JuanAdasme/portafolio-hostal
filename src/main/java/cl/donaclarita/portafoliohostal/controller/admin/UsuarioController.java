4780caca6c26724bc33cc1b3e0180076b267cddb
amant <amant@DESKTOP-J68G847>
2018-06-07 15:09:14 -0400

    Add model classes

--- src/main/java/cl/donaclarita/portafoliohostal/controller/admin/UsuarioController.java
+++ src/main/java/cl/donaclarita/portafoliohostal/controller/admin/UsuarioController.java
@@ -1,13 +1,24 @@
 package cl.donaclarita.portafoliohostal.controller.admin;
 
+import java.util.List;
+import java.util.logging.Logger;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import cl.donaclarita.portafoliohostal.model.Usuario;
+import cl.donaclarita.portafoliohostal.service.UsuarioService;
 
 @Controller
 public class UsuarioController {
 	
-	@GetMapping("admin/usuarios/list")
-	public String listUsers() {
-		return "admin/usuarios/list";
+	private final static Logger LOGGER = Logger.getLogger(EmpleadoController.class.getName());
+	UsuarioService restClient = new UsuarioService();
+	
+	@GetMapping("/admin/empleados/list")
+	public void listarTodos(Model model) {
+		List<Usuario> usuarios = restClient.findAllEmpleados();
+		model.addAttribute("usuarios", usuarios);
 	}
 }
