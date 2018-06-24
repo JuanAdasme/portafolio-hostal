5a57fbc11267930b25dcfe9d8bdd79cdc244efaa
amant <amant@DESKTOP-J68G847>
2018-06-24 19:02:05 -0400

    Agregar controladores y servicios

--- src/main/java/cl/donaclarita/portafoliohostal/controller/admin/UsuarioController.java
+++ src/main/java/cl/donaclarita/portafoliohostal/controller/admin/UsuarioController.java
@@ -6,6 +6,7 @@ import java.util.logging.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import cl.donaclarita.portafoliohostal.model.Usuario;
 import cl.donaclarita.portafoliohostal.service.UsuarioService;
@@ -20,5 +21,15 @@ public class UsuarioController {
 	public void listarTodos(Model model) {
 		List<Usuario> usuarios = restClient.findAllUsuarios();
 		model.addAttribute("usuarios", usuarios);
+	}
+	
+	@GetMapping("/admin/usuarios/add")
+	public void agregar() {
+	}
+	
+	@PostMapping("/admin/usuarios/add")
+	public void agregar(String username, String clave, String rol, String nombre, String apellidoPaterno, String apellidoMaterno) {
+		Usuario usuario = new Usuario(username, clave, rol, nombre, apellidoPaterno, apellidoMaterno);
+		restClient.crearUsuario(usuario);
 	}
 }
